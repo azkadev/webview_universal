dbf19eff22c6d319573d1beb157ddc7d53ee9050
￴ <>
2023-03-10 17:27:54 +0700

    update

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import path_provider_foundation
+import webview_universal
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  WebviewUniversalPlugin.register(with: registry.registrar(forPlugin: "WebviewUniversalPlugin"))
+}
---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <webview_universal/webview_universal_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  WebviewUniversalPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WebviewUniversalPlugin"));
+}
